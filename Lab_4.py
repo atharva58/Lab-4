ee4cadb76e52f7ff9efd064194c1160c1725653b
iamatharva58 <31650706+iamatharva58@users.noreply.github.com>
2020-05-11 16:30:54 -0300

    modified

--- Lab_4.py
+++ Lab_4.py
@@ -1,12 +1,32 @@
-#Task 1
-
-import string
-import math
-x=input("Enter a file name(.txt file): ")
+# file handling
+x=input("Enter a file name in .txt: \n")
 f_hand=open(x)
-for i in f_hand:
+
+def capital(a): #to print the file in capital letter and strip down the extra spaces
+  for i in f_hand:
     i=i.strip()
     z=i.lower()
-    z=z.strip(string.punctuation)
-    x+=i
-print(x)
+    print(z)
+
+def lst(b): # splitting the .txt file in list of strings or chars
+  bart=list()
+  x = []
+  #return(f_hand.read().strip().split()) #this is correct
+  for i in f_hand:
+    i=i.strip()
+    i=i.strip('\n')
+    i=i.split()
+    x += i
+  return(x)
+
+#print(lst(x))
+
+def count_char(c): #to count number of items in the list
+  count =0
+  temp=list()
+  temp=lst(x)
+  for i in temp:
+    count+=1
+  print(count)
+
+count_char(x)
